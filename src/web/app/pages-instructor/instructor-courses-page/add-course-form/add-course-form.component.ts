a0a244d0ca8748c3134e56c9b7e2932a6267107d
Kon Yoo <konyoo@wustl.edu>
2020-08-02 23:19:02 +0900

    instructor-help-getting started example box

--- src/web/app/pages-instructor/instructor-courses-page/add-course-form/add-course-form.component.ts
+++ src/web/app/pages-instructor/instructor-courses-page/add-course-form/add-course-form.component.ts
@@ -25,11 +25,20 @@ const formatTwoDigits: Function = (n: number): string => {
   templateUrl: './add-course-form.component.html',
   styleUrls: ['./add-course-form.component.scss'],
 })
+
 export class AddCourseFormComponent implements OnInit {
 
   @Input() isEnabled: boolean = true;
   @Output() courseAdded: EventEmitter<void> = new EventEmitter<void>();
   @Output() closeCourseFormEvent: EventEmitter<void> = new EventEmitter<void>();
+
+
+  // @ViewChildren('courseIdText, new-course-name, new-time-zone, btn-save-course,btn btn-primary') formElements:QueryList<ElementRef>;
+  // @ViewChild('courseIdText', { static: false }) courseIdText: ElementRef;
+  // @ViewChild('new-course-name');
+  // @ViewChild('new-time-zone')
+  // @ViewChild('btn-save-course')
+  // @ViewChild('btn btn-primary')
 
   timezones: Timezone[] = [];
   timezone: string = '';
@@ -37,9 +46,18 @@ export class AddCourseFormComponent implements OnInit {
   newCourseName: string = '';
   isAddingCourse: boolean = false;
 
+  // elementRef: ElementRef;
+
+  @Input() isExample: boolean = false; // *ngIf to check tab accessibility
+
   constructor(private statusMessageService: StatusMessageService,
               private courseService: CourseService,
-              private timezoneService: TimezoneService) { }
+              private timezoneService: TimezoneService,
+              // @Inject(ElementRef) elementRef: ElementRef
+              // private ref: ElementRef
+              // this.courseIdText = elementRef;
+               ) {  }
+
 
   ngOnInit(): void {
     for (const [id, offset] of Object.entries(this.timezoneService.getTzOffsets())) {
@@ -51,9 +69,33 @@ export class AddCourseFormComponent implements OnInit {
         offset: offset === 0 ? 'UTC' : `UTC ${sign}${formatTwoDigits(hourOffset)}:${formatTwoDigits(minOffset)}`,
       });
     }
+    // If the form is used as example,
+    // if(!this.isExample) {
+      // this.ref.nativeElement.focus();
+      //this is not working
+    // } else {
+      // this.ref.nativeElement.tabindex = 0;
+    // }
 
     this.timezone = this.timezoneService.guessTimezone();
   }
+
+  ngAfterViewInit() {
+    // setTimeout(() =>this.courseIdText.nativeElement.focus());
+    if(this.isExample){
+      var typeCourseId = document.getElementById("new-course-id");
+      var typeCourseName = document.getElementById("new-course-name");
+      var selectTimeZone = document.getElementById("new-time-zone");
+      var btnAutoDetect = document.getElementById("btn-auto-detect");
+      var btnAddCourse = document.getElementById("btn-save-course");
+      typeCourseId?.setAttribute('tabindex', '-1');
+      typeCourseName?.setAttribute('tabindex', '-1');
+      selectTimeZone?.setAttribute('tabindex', '-1');
+      btnAutoDetect?.setAttribute('tabindex', '-1');
+      btnAddCourse?.setAttribute('tabindex', '-1');
+    }
+  }
+
 
   /**
    * Auto-detects timezone for instructor.
@@ -100,4 +142,11 @@ export class AddCourseFormComponent implements OnInit {
   closeEditFormHandler(): void {
     this.closeCourseFormEvent.emit();
   }
+/*
+* Accessibility control
+*/
+  // setActiveTab(index: number) {
+  //   this.tabIndex = index;
+  // }
+
 }
